3597ccea7f958981e9be16b3df4e52338d99145c
Hugo Lamounier <hugolamo@gmail.com>
2023-05-27 23:53:30 -0300

    Init

---
+++ Ptah/ClientApp/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="h-screen bg-black">
+      <span className="text-white">Hello</span>
+    </div>
+  );
+}
+
+export default App;
